943c515c3d062ac0bfbe958a683090eba4c1f45a
Radim Simanek <ra.dim@me.com>
2022-07-07 18:50:39 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use blockchainlib::*;
+
+fn main () {
+    let mut block = Block::new(13, now(), vec![0;32], 1, "Genesis block".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("{:?}", &h);
+
+    
+    block.hash = h;
+    println!("{:?}", &block);
+}
